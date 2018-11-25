f2d04d7bd4b34b3d6f89bdab8de27b7011aa77a1
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-11-26 08:15:26 +0900

    [Update] First commit .

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// 標準入出力を行うライブラリ
+
+using namespace std;
+// std という名前空間の利用
+
+int main() {
+// 関数の宣言は function の代わりに int (整数型) というものを使う
+// かならず main という名前の関数を使ってプログラムを実行
+
+  cout << "Hello World!" << endl;
+  // cout はconsole output (コンソール出力) を表したオブジェクト
+  // cout に対して、 "Hello World!" とう文字列を渡す
+}
